9598951d57e7aeaa79031c86e84f9ef8d9aad78b
Aditi Acharya <aditiacharya2@gmail.com>
2026-06-25 12:42:02 +0545

    refactor: file structure

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { SectionLabel } from "./SectionLabel";
+import { SectionLabel } from "../SectionLabel";
 import "../../assets/styles/experience.css";
 import { COMMITS, TYPECOLORS } from "../../consts";
 
